7cebed770fc093c69f3151d843e70ce79cbddff6
ChoiseU <csw980928@gmail.com>
2024-12-10 19:43:26 +0900

    feat: terraform 구성

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -50,7 +50,7 @@ resource "aws_instance" "ec2_instances-app" {
   associate_public_ip_address = true
 
   tags = {
-    Name = "split-app-${count.index + 1}"
+    Name = "total-app-${count.index + 1}"
   }
 }
 
@@ -66,12 +66,12 @@ resource "aws_instance" "ec2_instances-mongo" {
   associate_public_ip_address = true
 
   tags = {
-    Name = "split-mongo-${count.index + 1}"
+    Name = "redis-${count.index + 1}"
   }
 }
 
 resource "aws_instance" "ec2_instances-redis" {
-  count         = 3
+  count         = 2
   ami           = "ami-040c33c6a51fd5d96"
   instance_type = "t3.small"
   key_name      = "stress-test-key"
@@ -82,77 +82,9 @@ resource "aws_instance" "ec2_instances-redis" {
   associate_public_ip_address = true
 
   tags = {
-    Name = "split-redis-${count.index + 1}"
+    Name = "mongo-${count.index + 1}"
   }
 }
-
-
-
-resource "aws_lb" "app_alb" {
-  name               = "total-app-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [data.aws_security_group.existing_sg.id]
-  subnets            = [data.aws_subnet.existing_subnet.id]
-
-  enable_deletion_protection = false
-  idle_timeout               = 60
-
-  tags = {
-    Name = "total-app-alb"
-  }
-}
-
-resource "aws_lb_target_group" "app_tg" {
-  name        = "total-app-tg"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = data.aws_vpc.existing_vpc.id
-  target_type = "instance"
-
-  health_check {
-    path                = "/health"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 3
-    unhealthy_threshold = 2
-    protocol            = "HTTP"
-  }
-
-  tags = {
-    Name = "total-app-tg"
-  }
-}
-
-resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.app_alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
-  }
-}
-
-locals {
-  all_app_instance_ids = aws_instance.ec2_instances-app[*].id
-}
-
-resource "aws_lb_target_group_attachment" "app_targets" {
-  count            = length(local.all_app_instance_ids)
-  target_group_arn = aws_lb_target_group.app_tg.arn
-  target_id        = local.all_app_instance_ids[count.index]
-  port             = 3000
-
-  depends_on = [aws_lb_listener.http_listener]
-}
-
-
-
-
-
-
 
 output "instance_public_ips_app" {
   description = "생성된 EC2 인스턴스의 퍼블릭 IP 목록"
